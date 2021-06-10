1158b81f3934546bd6637828541481234ba83115
Shynu Mary Varghese <Svarghese2938@conestogac.on.ca>
2021-06-10 07:26:15 +0530

    inline style according to user input

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const app = Vue.createApp({
     return {
       //store input whatever user enters
       inputClass: "",
+      paragraphIsVisible: true,
+      inputBackgroundColor: "",
     };
   },
-  method: {},
-  computed: {},
+  methods: {
+    toggleParagraphVisibility() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+  },
+  computed: {
+    paraClass() {
+      return {
+        user1: this.inputClass === "user1",
+        user2: this.inputClass === "user2",
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible,
+      };
+    },
+  },
 });
 app.mount("#assignment");
